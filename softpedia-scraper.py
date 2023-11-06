5b1522b4389f6201528a92c89a1a41152b17d91a
Md Rayhan Amin <rohan.cse.cuet10@gmail.com>
2023-11-06 15:58:56 -0600

    Added few lines on the scrapper for proxies

--- softpedia-scraper.py
+++ softpedia-scraper.py
@@ -8,6 +8,7 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.common.exceptions import *
 
+
 # prepare the option for the chrome driver
 options = webdriver.FirefoxOptions()
 options.add_argument('-headless')
@@ -22,9 +23,26 @@ def extract_num_pages(url):
     :param url:
     :return: number of pages
     """
-    soup = BeautifulSoup(requests.get(url).content, 'lxml')
-    last_page = soup.find(class_='fr ta_right').find_all('a')
-    return int(last_page[-2].text)
+
+    print(url)
+    driver = webdriver.Firefox(options=options)
+    driver.set_page_load_timeout(5)
+    driver.implicitly_wait(5)
+    driver.get(url)
+    soup = BeautifulSoup(driver.page_source, "html.parser")
+    last_page = soup.find(class_='fr ta_right')
+    print(last_page)
+    
+    if last_page:
+        page_links = last_page.find_all('a')
+        print(page_links)
+        a = int(page_links[-2].text)
+        print(a)
+        return int(page_links[-2].text)
+    else:
+        # Handle the case where the element is not found, e.g., by returning a default value.
+        return 1  # Default to 1 page if element is not found
+
 
 
 def scrape_app_features(url):
@@ -32,15 +50,17 @@ def scrape_app_features(url):
     :param url: app to be scraped
     :return: a list of requirements
     """
-    driver = webdriver.Firefox(executable_path=webdriver_path, options=options)
+    driver = webdriver.Firefox(options=options)
     driver.set_page_load_timeout(5)
     driver.implicitly_wait(5)
     driver.get(url)
+
+
     try:
         # switch to 'Feature' tab in the application's page
         feature_tab = driver.find_element_by_xpath('/html/body/div[1]/div[2]/div[2]/div[2]/div[1]/div[2]/h3[3]')
         feature_tab.click()
-        soup = BeautifulSoup(driver.page_source, 'lxml')
+        soup = BeautifulSoup(driver.page_source, "html.parser")
         # highlights can be features, system requirements, or others
         highlights = soup.find(id='specifications').find_all("b", class_='upcase bold')
         for item in highlights:
@@ -76,12 +96,18 @@ def scrape_all(path):
     urls = domains_df['url']
     reqs_output = domains_df['output']
 
+
+        
+
     for d, u, o in zip(domains, urls, reqs_output):
         # print(d, u, o, sp, ep)
         reqs_df = pd.DataFrame(columns=['domain', 'app', 'requirement', 'url'])
         num_pages = extract_num_pages(u.format(1))  # get number of pages from scraping the first page
         for page in range(1, num_pages+1):
-            r = requests.get(u.format(page))
+            ll = u.format(page)
+            proxy = 'http://107.1.93.222:80'
+            
+            r = requests.get(u.format(page), proxies={"http": proxy, "https": proxy})
             if r.status_code != 200:
                 print('can\'t retrieve the html!')
                 continue
